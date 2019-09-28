977de7d7635ccce79f072d3d59d18ae4182d71c9
Jackson Jayaraj <Jackson.Jayaraj@aexp.com>
2019-09-28 11:46:44 -0700

    Initial Commit

---
+++ iOS TipCalc Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS TipCalc Assignment
+//
+//  Created by Jackson Jayaraj on 9/28/19.
+//  Copyright © 2019 Jackson Jayaraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
